ba933e648a13e2b0605f17def84aa31bf91939c4
JnBanks <joku.banks@gmail.com>
2022-05-02 20:37:56 -0700

    still working on the server, routes and api files

---
+++ controllers/api.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const data = require("../db/db.json");
+const fs = require('fs');
+
+// fs.readFile("db/db.json","utf-8", (err, data) => {
+//     if (err) throw err;
+//     var notes = JSON.parse(data);
+
+// })
+// POST /api/notes for recieving new notes to save on the request body, add it to the db.json file, and then return the new note to the client
+router.get('/notes', (req, res) => {
+    console.log(data);
+    res.send(data)
+}
+);
+
+router.post("/notes", function(req, res) {
+    let newNote = req.body;
+
+} )
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,43 +1,25 @@
 const express = require('express');
-const path = require('path');
-//const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
-
+const app = express();
 const PORT = process.env.port || 3001;
 
-const app = express();
+//const fs = require('fs');
+const path = require('path');
 
-// Import custom middleware, "cLog"
-//app.use(clog);
+//const api = require('./public/index');
+
+const routes = require('./controllers/routes');
 
 // Middleware for parsing JSON and urlencoded form data
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/", routes)
 app.use('/api', api);
 
-app.use(express.static('public'));
+app.delete('/api/notes/:title', (req, res) => {
 
-// GET /notes for notes.html file
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/js/notes.html'))
-);
-
-// GET * for index.html file
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/js/index.html'))
-);
-
-// GET /api/notes for the db.json file to return all saved notes as JSON
-app.get('/api/notes', (req, res) =>
-  res.readFile(path.join(__dirname, 'db.json'))
-);
-
-// POST /api/notes for recieving new notes to save on the request body, add it to the db.json file, and then return the new note to the client
-app.post('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'db.json'))
-);
-
+})
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
